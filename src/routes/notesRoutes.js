b4b60f2c2f80a06301bc97db99b1cbe7a93f08ed
Ihor Kuzmenko <nameless1306@gmail.com>
2026-06-08 16:46:25 +0300

    correct mistakes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import {
-  getNotes,
-  getStudentById,
+  getAllNotes,
+  getNoteById,
   createNote,
   deleteNote,
   updateNote,
@@ -9,9 +9,9 @@ import {
 
 const router = Router();
 
-router.get('/notes', getNotes);
+router.get('/notes', getAllNotes);
 
-router.get('/notes/:noteId', getStudentById);
+router.get('/notes/:noteId', getNoteById);
 
 router.post('/notes', createNote);
 
