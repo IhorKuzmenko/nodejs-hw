cb2733ee1af637068e336a013e6da16995b02a79
Ihor Kuzmenko <nameless1306@gmail.com>
2026-06-08 16:32:06 +0300

    node.js hw 02-mongodb done

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import {
+  getNotes,
+  getStudentById,
+  createNote,
+  deleteNote,
+  updateNote,
+} from '../controllers/notesController.js';
+
+const router = Router();
+
+router.get('/notes', getNotes);
+
+router.get('/notes/:noteId', getStudentById);
+
+router.post('/notes', createNote);
+
+router.delete('/notes/:noteId', deleteNote);
+
+router.patch('/notes/:noteId', updateNote);
+
+export default router;
